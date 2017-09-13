38bf34670dfaf4f967d0a521450db0c23f9e9133
Regis <boudinot.regis@yahoo.com>
2017-09-13 17:45:06 -0400

    hubv alpha

---
+++ README.md
@@ -0,0 +1,19 @@
+# HubV
+
+Vue EventHub delegator!
+
+Alternative to Vuex using Vue's built in Hub system.
+
+### ES5
+
+Written in ES5 to reduce bundle size during UMD build step
+
+Exposed as a named UMD module for Node/DOM compatibility
+
+### API
+
+WIP
+
+### LICENSE
+
+Checkout the `LICENSE.md` file at the root of this Directory
---
+++ test/hubv.test.js
@@ -0,0 +1,39 @@
+const Vue = require('vue');
+const HubV = require('../lib');
+
+test('HubV can install correctly and can be called from Vue instance', () => {
+  Vue.use(HubV);
+
+  const vm = new Vue({});
+
+  expect(typeof vm.$hubv).toBe('object');
+});
+
+test('HubV can update parent state from child', () => {
+  Vue.use(HubV);
+
+  const vm = new Vue({
+    data() {
+      return {
+        msg: 'wow',
+      };
+    },
+    methods: {
+      capMsg() {
+        this.msg = this.msg.toUpperCase();
+      },
+    },
+    created() {
+      const { capMsg } = this;
+      this.$hubv.dispatchBuilder({ capMsg });
+    },
+    destroyed() {
+      const { capMsg } = this;
+      this.$hubv.dispatchDestroyer({ capMsg });
+    },
+  });
+
+  vm.$hubv.dispatch({ action: 'capMsg' });
+
+  expect(vm.msg).toBe('WOW');
+});
